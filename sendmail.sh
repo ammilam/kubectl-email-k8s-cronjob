6e2697430f73711e5033446f18d0a9bdd8b2aa2d
Andrew Milam <Andrew.Milam@davita.com>
2020-12-02 00:40:35 -0600

    email/kubectl implementation

--- sendmail.sh
+++ sendmail.sh
@@ -1,9 +1,10 @@
 #! /bin/bash -e
 
 BODY=$(kubectl get pods -A -o wide| egrep -v "(Running|Complete)")
-sendEmail -f test@davita.com \
+sendEmail -f k8s_is_sad_alert@davita.com \
 -t andrew.milam@davita.com \
+-t SunilSagar.Uppala@davita.com \
 -s mmnlb.davita.com:25 \
--u "Helllooooo, I am a cronjob with bad k8s news!" \
+-u " Helllooooo, I am a cronjob with bad [sea1sbx] k8s news!" \
 -m "These are the pods having problems:\n  \n$BODY" \
 -o tls=no
