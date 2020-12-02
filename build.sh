0a6655dc105d1b144a83187109089c8c1b302a48
Andrew Milam <andrew.milam@davita.com>
2020-12-02 06:45:41 +0000

    Delete build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-#!/bin/sh
-
-docker build -t dockerhub.artifactory.davita.com/cwow-grafana/test-email-cronjob:3.1 -f Dockerfile . --no-cache
-docker push dockerhub.artifactory.davita.com/cwow-grafana/test-email-cronjob:3.1
